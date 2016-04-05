83bb32ec40f1dac91422088ddf36bda07bdc9c76
Vladislav Marinov <vladi_im@abv.bg>
2016-04-05 13:09:12 +0300

    Added files via upload

---
+++ Employee.java
@@ -0,0 +1,69 @@
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String jobPosition;
+	private double salary;
+	private String department;
+
+	public Employee(String firstName, String lastName, String address,
+			String jobPosition, double salary, String department) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.jobPosition = jobPosition;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getJobPosition() {
+		return jobPosition;
+	}
+
+	public void setJobPosition(String jobPosition) {
+		this.jobPosition = jobPosition;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+}
